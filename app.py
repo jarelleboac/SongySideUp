c3b65a5d18c37bea53ff8fc580ee96bfac27bc93
jarelleboac <jarellefboac@gmail.com>
2021-01-23 11:47:40 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+#from data import Articles
+
+app = Flask(__name__)
+
+
+#Articles=Articles()
+@app.route("/")
+def hello():
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+"""@app.route('/articles')
+def articles():
+    return render_template('articles.html', articles=Articles)"""
+
+if __name__ == "__main__":
+    app.run(debug= True)
+    #just run once on terminal and just need to refresh browser
---
+++ templates/data.py
@@ -0,0 +1,31 @@
+def Articles():
+    articles= [
+        {
+            #each article in here
+            'id': 1,
+            'title': 'Article 1',
+            'body': 'Hi!!!!',
+            'author': 'Brad Traversy',
+            'create_date': '01-2021'
+
+        },
+        {
+            #each article in here
+            'id': 2,
+            'title': 'Article 2',
+            'body': 'Hi!!!!',
+            'author': 'Brad Traversy',
+            'create_date': '01-2021'
+
+        },
+        {
+            #each article in here
+            'id': 3,
+            'title': 'Article 3',
+            'body': 'Hi!!!!',
+            'author': 'Brad Traversy',
+            'create_date': '01-2021'
+
+        }
+    ]
+    return articles
